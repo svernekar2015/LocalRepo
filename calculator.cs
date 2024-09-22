dbb657eedf9e29a0961353dcecd4b38ce3864cab
svernekar2015 <sagarvernekar2015@gmail.com>
2024-09-22 19:48:29 +0530

    added new program.cs & modified calculor.cs

--- calculator.cs
+++ calculator.cs
@@ -1,11 +1,11 @@
 public class Calculator
 {
-    public int add(int a, int b)
+    public static int add(int a, int b)
     {
         return a+b;
     }
 
-    public int subtract(int a, int b)
+    public static int subtract(int a, int b)
     {
         if(a > b)
         {
@@ -18,8 +18,14 @@ public class Calculator
 
     }
 
-    public int multiply(int a, int b)
+    public static int multiply(int a, int b)
     {
         return a*b;
+    }
+
+    public static int divide(int a, int b)
+    {
+        if (a != 0 || b != 0)
+        return a/b;
     }
 }
